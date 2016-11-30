08e153288255c6608b9b9fc30e5fb307735e85a7
rickuster <rickuster@gmail.com>
2016-11-30 00:42:25 -0800

    DOM scripting click event assignment completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
      ]
  };
 
+var albumFettuccine = {
+     title: 'The Meatball',
+     artist: 'Big Sphagetti',
+     label: 'BS',
+     year: '2000',
+     albumArtUrl: 'assets/images/album_covers/09.png',
+     songs: [
+         { title: 'Hello, Alfredo?', duration: '2:01' },
+         { title: 'Slurp, slurp, slurp', duration: '6:01' },
+         { title: 'Fits on your plate', duration: '4:21'},
+         { title: 'Can you eat me now?', duration: '5:14' },
+         { title: 'Wrong sauce', duration: '1:15'}
+     ]
+ };
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -40,23 +55,20 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-  
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
     
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     
      albumSongList.innerHTML = '';
- 
      
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
@@ -65,4 +77,14 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     var albums = [albumPicasso, albumMarconi, albumFettuccine];
+     var index = 1;
+     
+     albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]); 
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+     });
  };
