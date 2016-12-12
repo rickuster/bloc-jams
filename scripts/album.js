f1e46b0859aca08717ac785afc3c7fde26014757
rickuster <rickuster@gmail.com>
2016-12-11 22:44:08 -0800

    Completed dom scripting play/pause part 2 assignment

--- scripts/album.js
+++ scripts/album.js
@@ -65,11 +65,23 @@ var setCurrentAlbum = function(album) {
 
 var findParentByClassName = function(element, targetClass) {
     if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
+        if (element.parentElement){
+            var currentParent = element.parentElement;
+            while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
+            }
         }
-        return currentParent;
+        else {
+            alert("No parent found");
+        }    
+        
+        if (currentParent == null){
+            alert("No parent found with that class name");
+        }
+        else {
+            return currentParent;
+        }
+        
     }
 };
 
